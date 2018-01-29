3fa16766bd4aa933c1b6b43a2d40b31fd489f5b3
devjanaprime <dev@primeacademy.io>
2018-01-29 11:16:21 -0600

    after first short lecture

--- gacruxReview.js
+++ gacruxReview.js
@@ -1,1 +1,14 @@
 console.log( 'js' );
+
+var records = 123;
+
+function aboutTheCollection(){
+  console.log( 'in aboutTheCollection', records );
+  return records;
+} // end aboutTheCollection
+
+function addARecord(){
+  console.log( 'in addARecord');
+  records++;
+  return true;
+} // addARecord
