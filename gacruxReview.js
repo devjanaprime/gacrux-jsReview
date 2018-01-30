9916e186e7d9417418dff5e5802a906728df24b9
devjanaprime <dev@primeacademy.io>
2018-01-30 11:11:12 -0600

    ver 0.3

--- gacruxReview.js
+++ gacruxReview.js
@@ -1,5 +1,26 @@
 var records = [];
 
+var prototypeRecord = {
+  artist: 'Kendrick Lamar',
+  title: 'DAMN',
+  year: 2017,
+  tracklist: [ [ 'BLOOD', 'DNA', 'YAH', 'ELEMENT', 'FEEL', 'LOYALTY', 'PRIDE' ], [ 'HUMBLE', 'LUST', 'LOVE', 'XXX', 'FEAR', 'GOD', 'DUCKWORTH' ] ]
+}
+
+var test1 = {
+  artist: 'Childish Gambino',
+  title: 'Awaken My Love,!',
+  year: 2016,
+  tracklist: [ [ 'How I Met Your Mother', 'Have Some Love', 'Boogieman', 'Zombies', 'Riot' ], ['Redbone', 'California', 'Terrified', 'Babyboy', 'The Night', 'Stand Tall' ] ]
+}
+
+var test2 = {
+  artist: 'Chance The Rapper',
+  title: 'Coloring Book',
+  year: 2016,
+  tracklist: [ [ 'All We Got', 'No Problem', 'Summer Friends', 'Dram', 'Blessings' ], [ 'Same Drugs', 'Mix Tape', 'Angels', 'Juke Jam', 'All Night', 'How Great', 'Smoke Break', 'Finish Line/Down' ] ]
+}
+
 // to illustrate the value of returning a boolean from a function:
 function addRecordExample(){
   if( addToCollection( 1 ) ){
